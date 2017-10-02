1504f65e9cf8974c546fe9c6d176a1861482d1e6
chris-wegner <chris.wegner@gmail.com>
2017-10-02 16:35:26 -0500

    Adding a ScorecardSite that renders a stub of a React page.

---
+++ ScorecardSite/wwwroot/js/index.jsx
@@ -0,0 +1,13 @@
+﻿var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Hello, world!
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('content')
+);
